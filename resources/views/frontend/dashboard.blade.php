89f4e89864d8ef89702287771843790b9be07dc3
Romyadhy <romyadhy@student.undiksha.ac.id>
2025-09-25 15:52:42 +0800

    set up breeze, tailwindcss, and some pages

---
+++ resources/views/frontend/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="bg-gray-100 dark:bg-gray-900 min-h-screen flex items-center justify-center">
+        <h1 class="text-6xl font-bold text-teal-500 ">BIRJON KERENLAH</h1>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('frontend.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/tickets', function () {
+    return view('frontend.tickets');
+})->middleware(['auth', 'verified'])->name('tickets');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
